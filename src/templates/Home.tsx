417b88b4a35ca38779d3e5eefad8a93833baecfe
Melvice999 <rotiqu51191@gmail.com>
2024-04-21 20:53:17 +0700

    adding profil picture

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -5,11 +5,13 @@ export default function Home() {
     <div className="w-full min-h-screen flex items-center">
       <div className="w-full flex flex-row-reverse items-center justify-between mx-72 max-xl:mx-20 max-lg:mx-10 max-md:block max-md:mx-5">
         <div className="flex justify-center items-center ms-20 max-lg:ms-10 max-md:ms-0 max-md:mt-16">
-          <div className="bg-gray-300 h-64 w-64 rounded-full"></div>
+          <div className="bg-gray-300 h-64 w-64 rounded-full border-red-500 border-2 overflow-hidden">
+            <img src="img/pas-foto.png" alt="" />
+          </div>
         </div>
         <div className="flex flex-col justify-center items-center gap-y-3 max-md:mt-10 ">
           <h1 className="uppercase text-center text-outline-transparent">hello, my name is</h1>
-          
+
           <div className="capitalize flex items-center text-green-600 text-4xl font-bold font-josefin mb-2">
             <div className="text-8xl max-lg:text-6xl max-md:text-5xl font-extrabold text-nowrap">m. syakirin</div>
           </div>
